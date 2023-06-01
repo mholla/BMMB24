cd305b6adbfd7256d4ad6d416a72ac0fb2bc8aef
Jack Consolini <jconsol2@nd.edu>
2023-06-01 12:32:33 -0400

    Add files via upload

--- analysis/curvature and cut length/analysis_curvature_cutlength.py
+++ analysis/curvature and cut length/analysis_curvature_cutlength.py
@@ -15,7 +15,7 @@ def polyRegFit(x, a, b, c, d, e, f):
     polyReg = a*(x**5)+b*(x**4)+c*(x**3)+d*(x**2)+e*(x)+f
     return polyReg
 
-def plot(odb1, odb2, odb3, odb4, odb5, xticks, yticks, legend, name, colors):
+def plot(path, odb1, odb2, odb3, odb4, odb5, xticks, yticks, legend, name, colors):
     cmap = matplotlib.colormaps["Blues"]
     
     # figure settings   
@@ -27,12 +27,12 @@ def plot(odb1, odb2, odb3, odb4, odb5, xticks, yticks, legend, name, colors):
     plt.rcParams['font.size'] = 13
     
     # extract data
-    dataframe1 = pd.read_csv(odb1, encoding='ISO-8859-1')
-    dataframe2 = pd.read_csv(odb2, encoding='ISO-8859-1')
-    dataframe3 = pd.read_csv(odb3, encoding='ISO-8859-1')
-    dataframe4 = pd.read_csv(odb4, encoding='ISO-8859-1')
-    dataframe5 = pd.read_csv(odb5, encoding='ISO-8859-1')
-    dataframeHenderson = pd.read_csv('../simulations/curvature and cut length .csv files/Henderson_2005_Flat_case.csv', encoding='ISO-8859-1')
+    dataframe1 = pd.read_csv(path+odb1, encoding='ISO-8859-1')
+    dataframe2 = pd.read_csv(path+odb2, encoding='ISO-8859-1')
+    dataframe3 = pd.read_csv(path+odb3, encoding='ISO-8859-1')
+    dataframe4 = pd.read_csv(path+odb4, encoding='ISO-8859-1')
+    dataframe5 = pd.read_csv(path+odb5, encoding='ISO-8859-1')
+    dataframeHenderson = pd.read_csv(path+'Henderson_2005_Flat_case.csv', encoding='ISO-8859-1')
     
     # curve fit data so that graph properly ends at 1.14 stretch
     # note: the reason the graph does not start from zero is because the nodes are not directly aligned prior to deformation
@@ -92,56 +92,60 @@ def plot(odb1, odb2, odb3, odb4, odb5, xticks, yticks, legend, name, colors):
     plt.savefig(name, dpi=400)
 
 def transverseCurvatureVsCutOpening():
-    odba2Tran  = '../simulations/curvature and cut length .csv files/a=2MouseDuraMaterTranCutResults.csv'
-    odba4Tran  = '../simulations/curvature and cut length .csv files/a=4MouseDuraMaterTranCutResults.csv'
-    odba6Tran  = '../simulations/curvature and cut length .csv files/a=6MouseDuraMaterTranCutResults.csv'
-    odba8Tran  = '../simulations/curvature and cut length .csv files/a=8MouseDuraMaterTranCutResults.csv'
-    odba10Tran = '../simulations/curvature and cut length .csv files/a=10MouseDuraMaterTranCutResults.csv'
+    path = '../../simulations/curvature and cut length .csv files/'
+    odba2Tran  = 'a=2MouseDuraMaterTranCutResults.csv'
+    odba4Tran  = 'a=4MouseDuraMaterTranCutResults.csv'
+    odba6Tran  = 'a=6MouseDuraMaterTranCutResults.csv'
+    odba8Tran  = 'a=8MouseDuraMaterTranCutResults.csv'
+    odba10Tran = 'a=10MouseDuraMaterTranCutResults.csv'
     legend = [r'$a=2$', r'$a=4$', r'$a=6$', r'$a=8$', r'$a=10$', 'Henderson et al.']
     name = 'figure-curvature_transverse.png'
     xticks = np.arange(1.000, 1.160, step=0.020)
     yticks = np.arange(0.000, 0.500, step=0.050)
     colors = [0.25, 0.40, 0.55, 0.70, 0.85, 1.00]
-    plot(odba2Tran, odba4Tran, odba6Tran, odba8Tran, odba10Tran, xticks, yticks, legend, name, colors)
+    plot(path, odba2Tran, odba4Tran, odba6Tran, odba8Tran, odba10Tran, xticks, yticks, legend, name, colors)
 
 def longitudinalCurvatureVsCutOpening():
-    odba2Long  = '../simulations/curvature and cut length .csv files/a=2MouseDuraMaterLongCutResults.csv'
-    odba4Long  = '../simulations/curvature and cut length .csv files/a=4MouseDuraMaterLongCutResults.csv'
-    odba6Long  = '../simulations/curvature and cut length .csv files/a=6MouseDuraMaterLongCutResults.csv'
-    odba8Long  = '../simulations/curvature and cut length .csv files/a=8MouseDuraMaterLongCutResults.csv'
-    odba10Long = '../simulations/curvature and cut length .csv files/a=10MouseDuraMaterLongCutResults.csv'
+    path = '../../simulations/curvature and cut length .csv files/'
+    odba2Long  = 'a=2MouseDuraMaterLongCutResults.csv'
+    odba4Long  = 'a=4MouseDuraMaterLongCutResults.csv'
+    odba6Long  = 'a=6MouseDuraMaterLongCutResults.csv'
+    odba8Long  = 'a=8MouseDuraMaterLongCutResults.csv'
+    odba10Long = 'a=10MouseDuraMaterLongCutResults.csv'
     legend = [r'$a=2$', r'$a=4$', r'$a=6$', r'$a=8$', r'$a=10$', 'Henderson et al.']
     name = 'figure-curvature_longitudinal.png'
     xticks = np.arange(1.000, 1.160, step=0.020)
     yticks = np.arange(0.000, 0.500, step=0.050)
     colors = [0.25, 0.40, 0.55, 0.70, 0.85, 1.00]
-    plot(odba2Long, odba4Long, odba6Long, odba8Long, odba10Long, xticks, yticks, legend, name, colors)
+    plot(path, odba2Long, odba4Long, odba6Long, odba8Long, odba10Long, xticks, yticks, legend, name, colors)
 
 def transverseCutLengthVsCutOpening():
-    odbHalfTran = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterTranCutHalfAxisResults.csv'
-    odb4thTran  = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterTranCut4thAxisResults.csv'
-    odb8thTran  = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterTranCut8thAxisResults.csv'
-    odb16thTran = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterTranCut16thAxisResults.csv'
-    odb32ndTran = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterTranCut32ndAxisResults.csv'
+    path = '../../simulations/curvature and cut length .csv files/'
+    odbHalfTran = 'adultICRMouseDuraMaterTranCutHalfAxisResults.csv'
+    odb4thTran  = 'adultICRMouseDuraMaterTranCut4thAxisResults.csv'
+    odb8thTran  = 'adultICRMouseDuraMaterTranCut8thAxisResults.csv'
+    odb16thTran = 'adultICRMouseDuraMaterTranCut16thAxisResults.csv'
+    odb32ndTran = 'adultICRMouseDuraMaterTranCut32ndAxisResults.csv'
     legend = [r'$l_0=b/2$', r'$l_0=b/4$', r'$l_0=b/8$', r'$l_0=b/16$', r'$l_0=b/32$']
     name = 'figure-cutLength_transverse.png'
     xticks = np.arange(1.000, 1.160, step=0.020)
     yticks = np.arange(0.000, 0.500, step=0.050)
     colors = [1.00, 0.80, 0.60, 0.40]
-    plot(odbHalfTran, odb4thTran, odb8thTran, odb16thTran, odb32ndTran, xticks, yticks, legend, name, colors)
+    plot(path, odbHalfTran, odb4thTran, odb8thTran, odb16thTran, odb32ndTran, xticks, yticks, legend, name, colors)
 
 def longitudinalCutLengthVsCutOpening():
-    odbHalfLong = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterLongCutHalfAxisResults.csv'
-    odb4thLong  = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterLongCut4thAxisResults.csv'
-    odb8thLong  = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterLongCut8thAxisResults.csv'
-    odb16thLong = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterLongCut16thAxisResults.csv'
-    odb32ndLong = '../simulations/curvature and cut length .csv files/adultICRMouseDuraMaterLongCut32ndAxisResults.csv'
+    path = '../../simulations/curvature and cut length .csv files/'
+    odbHalfLong = 'adultICRMouseDuraMaterLongCutHalfAxisResults.csv'
+    odb4thLong  = 'adultICRMouseDuraMaterLongCut4thAxisResults.csv'
+    odb8thLong  = 'adultICRMouseDuraMaterLongCut8thAxisResults.csv'
+    odb16thLong = 'adultICRMouseDuraMaterLongCut16thAxisResults.csv'
+    odb32ndLong = 'adultICRMouseDuraMaterLongCut32ndAxisResults.csv'
     legend = [r'$l_0=a/2$', r'$l_0=a/4$', r'$l_0=a/8$', r'$l_0=a/16$', r'$l_0=a/32$']
     name = 'figure-cutLength_longitudinal.png'
     xticks = np.arange(1.000, 1.160, step=0.020)
     yticks = np.arange(0.000, 0.500, step=0.050)
     colors = [1.00, 0.80, 0.60, 0.40]
-    plot(odbHalfLong, odb4thLong, odb8thLong, odb16thLong, odb32ndLong, xticks, yticks, legend, name, colors)
+    plot(path, odbHalfLong, odb4thLong, odb8thLong, odb16thLong, odb32ndLong, xticks, yticks, legend, name, colors)
 
 if __name__ == '__main__':
     
